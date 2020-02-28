8f59b090b99b06c0c16cc07d29d8adf4367be980
Benjamin Lewis <23inhouse@gmail.com>
2020-02-28 18:42:44 +0100

    Update home route to show error state

--- ocr/app.py
+++ ocr/app.py
@@ -11,13 +11,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    try:
-        img_url = request.args.get('img')
-        text = OCR(img_url).text()
-        html = HtmlPage().render(img_url, text)
-        return html
-    except ValueError:
+    img_url = request.args.get('img')
+    if img_url == None or img_url == '':
         return "<p>Please ensure there is a valid img arg</p><p>Example: <a href=\"http://0.0.0.0:5000/?img=https://i.stack.imgur.com/vrkIj.png\">http://0.0.0.0:5000/?img=https://i.stack.imgur.com/vrkIj.png</a>"
+
+    text = OCR(img_url).text()
+    html = HtmlPage().render(img_url, text)
+    return html
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
