848624f4bc16c0a82327a19f05bd8274859bb6af
Benjamin Lewis <23inhouse@gmail.com>
2020-02-28 16:16:16 +0100

    Add url img paramater

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import flask
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hey, we have Flask in a Docker container!'
+    img_url = request.args.get('img')
+    title = 'Your image url is {}'.format(img_url)
+    html = '<p>{}</p><p><img src="{}"/></p>'.format(title, img_url)
+    return html
 
 
 if __name__ == '__main__':
